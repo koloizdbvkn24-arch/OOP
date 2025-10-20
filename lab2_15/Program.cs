7c4f803b8d1b694ca9534a4b3c22e80fa8cfa6b2
koloizdbvkn24-archexport <koloizd.b.v_kn24@rcit.ukr.education>
2025-10-20 12:05:07 +0300

    first commit

---
+++ lab2_15/Program.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Reflection;
+namespace lab2_15
+{
+    class Song
+    {
+        private string title = string.Empty;
+        private string artist = string.Empty;
+
+        public string Title
+        {
+            get { return title; }
+            set
+            {
+                if (value == null || value.Length < 3)
+                {
+                    Console.WriteLine("Назва пісні має мати більш ніж 3 символи");
+                }
+                else
+                {
+                    title = value;
+                }
+            }
+        }
+
+        public string Artist
+        {
+            get { return artist; }
+            set
+            {
+                if (value == null || value.Length < 2)
+                {
+                    Console.WriteLine("Назва виконавця має мати більш ніж 2 символи");
+                }
+                else
+                {
+                    artist = value;
+                }
+            }
+        }
+
+        public Song(string T, string A)
+        {
+            Title = T;
+            Artist = A;
+        }
+
+        public override string ToString()
+        {
+            return $"{title} - {artist}";
+        }
+    }
+    class Playlist
+    {
+        private List<Song> songs = new List<Song>();
+
+        public int Length => songs.Count;
+
+        public Song this[int index]
+        {
+            get
+            {
+                if (index < 0 || index >= songs.Count)
+                {
+                    throw new IndexOutOfRangeException("Індекс виходить за межі списку");
+                }
+                return songs[index];
+            }
+            set
+            {
+                if (index < 0 || index >= songs.Count)
+                {
+                    throw new IndexOutOfRangeException("Індекс виходить за межі списку");
+                }
+                songs[index] = value;
+            }
+        }
+
+        public Song this[string title]
+        {
+            get
+            {
+                foreach (var song in songs)
+                {
+                    if (song.Title == title)
+                    {
+                        return song;
+                    }
+                }
+                throw new ArgumentException("Пісня з такою назвою не знайдена");
+            }
+        }
+
+        public static Playlist operator +(Playlist p, Song s)
+        {
+            p.songs.Add(s);
+            return p;
+        }
+
+        public static Playlist operator -(Playlist p, int index)
+        {
+            if (index < 0 || index >= p.songs.Count)
+            {
+                throw new IndexOutOfRangeException("Індекс виходить за межі списку");
+            }
+            p.songs.RemoveAt(index);
+            return p;
+            
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Playlist playlist = new Playlist();
+            Song song1 = new Song("Come as You Are", "Nirvana");
+            Song song2 = new Song("Billie Jean", "Michael Jackson");
+            Song song3 = new Song("Smells Like Teen Spirit", "Nirvana");
+            playlist += song1;
+            playlist += song2;
+            playlist += song3;
+
+            for (int i = 0; i < playlist.Length; i++)
+            {
+                Console.WriteLine($"Пісня {i + 1}: {playlist[i]}");
+            }
+
+            Console.WriteLine($"Було видаленно пісню {playlist[2]}");
+            playlist -= 2;
+
+            for (int i = 0; i < playlist.Length; i++)
+            {
+                Console.WriteLine($"Пісня {i + 1}: {playlist[i]}");
+            }
+
+            Console.WriteLine($"Довжина плейлиста: {playlist.Length}");
+
+        }
+    }
+}
---
+++ lab3_15/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RobotsApp
+{
+    // Базовий клас Robot
+    class Robot
+    {
+        public string Name { get; set; }
+
+        // Конструктор базового класу
+        public Robot(string name)
+        {
+            Name = name;
+        }
+
+        // Віртуальний метод, який перевизначатимуть похідні класи
+        public virtual void Work()
+        {
+            Console.WriteLine($"{Name} виконує загальну роботу.");
+        }
+    }
+
+    // Похідний клас CleanerRobot
+    class CleanerRobot : Robot
+    {
+        public CleanerRobot(string name) : base(name) // виклик конструктора базового класу
+        {
+        }
+
+        // Перевизначення методу Work
+        public override void Work()
+        {
+            Console.WriteLine($"{Name} прибирає приміщення.");
+        }
+    }
+
+    // Похідний клас DeliveryRobot
+    class DeliveryRobot : Robot
+    {
+        public DeliveryRobot(string name) : base(name)
+        {
+        }
+
+        // Перевизначення методу Work
+        public override void Work()
+        {
+            Console.WriteLine($"{Name} доставляє посилки.");
+        }
+    }
+
+    // Демонстрація поліморфізму
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Створюємо список роботів
+            List<Robot> robots = new List<Robot>
+            {
+                new CleanerRobot("RoboCleaner-01"),
+                new DeliveryRobot("DeliverBot-07"),
+                new Robot("SimpleBot")
+            };
+
+            // Поліморфізм кожен робот виконує власну дію через один метод Work()
+            foreach (var robot in robots)
+            {
+                robot.Work();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
+
